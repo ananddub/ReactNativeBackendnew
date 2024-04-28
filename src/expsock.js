c2626a7bb2b8b179586647f5b37739a471aaf367
ananddub <duanand6@gmail.com>
2024-04-28 14:55:23 +0530

    Add Error define io.Error

--- src/expsock.js
+++ src/expsock.js
@@ -286,6 +286,12 @@ io.on("connection", function (socket) {
         console.log("A user disconnected", dbActive);
     });
 });
+io.engine.on("connection_error", function (err) {
+    console.log(err.req); // the request object
+    console.log(err.code); // the error code, for example 1
+    console.log(err.message); // the error message, for example "Session ID unknown"
+    console.log(err.context); // some additional error context
+});
 // const PORT = process.env.PORT || 443;
 httpServer.listen(3000, function () {
     console.log("server is running on port localhost:4000");
--- src/expsock.ts
+++ src/expsock.ts
@@ -209,6 +209,13 @@ io.on("connection", (socket) => {
     });
 });
 
+io.engine.on("connection_error", (err) => {
+    console.log(err.req);      // the request object
+    console.log(err.code);     // the error code, for example 1
+    console.log(err.message);  // the error message, for example "Session ID unknown"
+    console.log(err.context);  // some additional error context
+});
+
 // const PORT = process.env.PORT || 443;
 httpServer.listen(3000, () => {
     console.log("server is running on port localhost:4000");
