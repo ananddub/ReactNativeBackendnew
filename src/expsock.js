74eb2a143edc69c6bf3dee65c22b2e66c8a47808
ananddub <duanand6@gmail.com>
2024-04-28 21:40:49 +0530

    Added Process.env module

--- src/expsock.js
+++ src/expsock.js
@@ -47,6 +47,120 @@ var io = new socket_io_1.Server(httpServer, {
     }
 });
 var dbActive = [];
+io.on("connection", function (socket) {
+    console.log("A user connected");
+    socket.on("register", function (response) {
+        removeDup(dbActive, response.admno);
+        if (response.admno !== undefined) {
+            dbActive.push({
+                admno: response.admno,
+                socketid: socket.id,
+            });
+        }
+        console.log("active user", dbActive);
+    });
+    socket.on("seen", function (response) {
+        var insert = "INSERT INTO userAnnoucment (admno,name,messaged,messageid) VALUES ('".concat(response.admno, "','").concat(response.name, "','").concat(response.message, "','").concat(response.messageid, "');");
+        sqlQueryUpdate(insert);
+    });
+    socket.on('getchat', function (response) { return __awaiter(void 0, void 0, void 0, function () {
+        var admno, usersel, _a, seen, unseen;
+        return __generator(this, function (_b) {
+            switch (_b.label) {
+                case 0:
+                    admno = "SELECT  a.messageid, a.message, a.receiver, a.sender, a.date , a.time  FROM adminAnnoucment a\n        LEFT JOIN userAnnoucment u ON a.messageid = u.messageid\n        WHERE (a.receiver = '".concat(response.admno, "' OR a.receiver = 'all') ORDER BY a.messageid DESC;;\n                        ");
+                    usersel = "SELECT  a.messageid, a.message, a.receiver, a.sender, a.date , a.time  FROM adminAnnoucment a\n        LEFT JOIN userAnnoucment u ON a.messageid = u.messageid\n        WHERE (a.receiver = '".concat(response.admno, "' OR a.receiver = 'all') \n          AND u.messageid IS NULL ORDER BY a.messageid DESC;\n          ");
+                    return [4 /*yield*/, Promise.all([sqlQueryStatus(admno), sqlQueryStatus(usersel)])];
+                case 1:
+                    _a = _b.sent(), seen = _a[0], unseen = _a[1];
+                    console.log(seen, unseen);
+                    socket.emit('getchat', { seen: seen.data, unseen: unseen.data });
+                    return [2 /*return*/];
+            }
+        });
+    }); });
+    socket.on('getlength', function (response) { return __awaiter(void 0, void 0, void 0, function () {
+        var usersel, unseen;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    usersel = "SELECT  COUNT(a.messageid) as c FROM adminAnnoucment a\n        LEFT JOIN userAnnoucment u ON a.messageid = u.messageid\n        WHERE (a.receiver = '".concat(response.admno, "' OR a.receiver = 'all') \n          AND u.messageid IS NULL ORDER BY a.messageid DESC;\n          ");
+                    return [4 /*yield*/, Promise.all([sqlQueryStatus(usersel)])];
+                case 1:
+                    unseen = (_a.sent())[0];
+                    console.log("length :", unseen.data[0].c);
+                    socket.emit('getlength', { unseen: unseen.data[0].c });
+                    return [2 /*return*/];
+            }
+        });
+    }); });
+    socket.on("admin", function (response) { return __awaiter(void 0, void 0, void 0, function () {
+        var _i, _a, admno, insert, i, j;
+        return __generator(this, function (_b) {
+            console.log(response.message);
+            if (Array.isArray(response.reciver) === true) {
+                for (_i = 0, _a = response.reciver; _i < _a.length; _i++) {
+                    admno = _a[_i];
+                    insert = "INSERT INTO adminAnnoucment (message,sender, receiver) VALUES ('".concat(response.message, "','").concat(response.sender, "','").concat(admno, "');");
+                    sqlQueryUpdate(insert);
+                }
+            }
+            else {
+                console.log('send aa array');
+                return [2 /*return*/];
+            }
+            if (response.reciver[0] === "all") {
+                console.log('emited :', response.message);
+                io.emit("notice", { "message": response.message });
+            }
+            else {
+                for (i = 0; i < response.reciver.length; i++) {
+                    for (j = 0; j < dbActive.length; j++) {
+                        if (dbActive[j].admno === response.reciver[i]) {
+                            console.log(j, 'passed ', dbActive[j]);
+                            io.to(dbActive[j].socketid).emit("notice", response.message);
+                        }
+                    }
+                }
+            }
+            return [2 /*return*/];
+        });
+    }); });
+    socket.on("disconnect", function () {
+        for (var i = 0; i < dbActive.length; i++) {
+            if (dbActive[i].socketid == socket.id) {
+                dbActive.splice(i, 1);
+            }
+        }
+        console.log("A user disconnected", dbActive);
+    });
+});
+io.engine.on("connection_error", function (err) {
+    console.log(err.req); // the request object
+    console.log(err.code); // the error code, for example 1
+    console.log(err.message); // the error message, for example "Session ID unknown"
+    console.log(err.context); // some additional error context
+});
+var PORT = process.env.PORT || 443;
+httpServer.listen(PORT,"0.0.0.0", function () {
+    console.log("server is running on port localhost:434");
+});
+function getLength(admno) {
+    return __awaiter(this, void 0, void 0, function () {
+        var usersel, unseen;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    usersel = "SELECT  COUNT(a.messageid) as c FROM adminAnnoucment a\n    LEFT JOIN userAnnoucment u ON a.messageid = u.messageid\n    WHERE (a.receiver = '".concat(admno, "' OR a.receiver = 'all') \n      AND u.messageid IS NULL ORDER BY a.messageid DESC;");
+                    return [4 /*yield*/, Promise.all([sqlQueryStatus(usersel)])];
+                case 1:
+                    unseen = (_a.sent())[0];
+                    console.log("length :", unseen.data[0].c);
+                    return [2 /*return*/, unseen.data[0].c];
+            }
+        });
+    });
+}
 function removeDup(arr, admno) {
     for (var i = 0; i < arr.length; i++) {
         if (arr[i].admno === admno) {
@@ -183,117 +297,3 @@ function sqlQueryStatus(query) {
         });
     });
 }
-function getLength(admno) {
-    return __awaiter(this, void 0, void 0, function () {
-        var usersel, unseen;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    usersel = "SELECT  COUNT(a.messageid) as c FROM adminAnnoucment a\n    LEFT JOIN userAnnoucment u ON a.messageid = u.messageid\n    WHERE (a.receiver = '".concat(admno, "' OR a.receiver = 'all') \n      AND u.messageid IS NULL ORDER BY a.messageid DESC;");
-                    return [4 /*yield*/, Promise.all([sqlQueryStatus(usersel)])];
-                case 1:
-                    unseen = (_a.sent())[0];
-                    console.log("length :", unseen.data[0].c);
-                    return [2 /*return*/, unseen.data[0].c];
-            }
-        });
-    });
-}
-io.on("connection", function (socket) {
-    console.log("A user connected");
-    socket.on("register", function (response) {
-        removeDup(dbActive, response.admno);
-        if (response.admno !== undefined) {
-            dbActive.push({
-                admno: response.admno,
-                socketid: socket.id,
-            });
-        }
-        console.log("active user", dbActive);
-    });
-    socket.on("seen", function (response) {
-        var insert = "INSERT INTO userAnnoucment (admno,name,messaged,messageid) VALUES ('".concat(response.admno, "','").concat(response.name, "','").concat(response.message, "','").concat(response.messageid, "');");
-        sqlQueryUpdate(insert);
-    });
-    socket.on('getchat', function (response) { return __awaiter(void 0, void 0, void 0, function () {
-        var admno, usersel, _a, seen, unseen;
-        return __generator(this, function (_b) {
-            switch (_b.label) {
-                case 0:
-                    admno = "SELECT  a.messageid, a.message, a.receiver, a.sender, a.date , a.time  FROM adminAnnoucment a\n        LEFT JOIN userAnnoucment u ON a.messageid = u.messageid\n        WHERE (a.receiver = '".concat(response.admno, "' OR a.receiver = 'all') ORDER BY a.messageid DESC;;\n                        ");
-                    usersel = "SELECT  a.messageid, a.message, a.receiver, a.sender, a.date , a.time  FROM adminAnnoucment a\n        LEFT JOIN userAnnoucment u ON a.messageid = u.messageid\n        WHERE (a.receiver = '".concat(response.admno, "' OR a.receiver = 'all') \n          AND u.messageid IS NULL ORDER BY a.messageid DESC;\n          ");
-                    return [4 /*yield*/, Promise.all([sqlQueryStatus(admno), sqlQueryStatus(usersel)])];
-                case 1:
-                    _a = _b.sent(), seen = _a[0], unseen = _a[1];
-                    console.log(seen, unseen);
-                    socket.emit('getchat', { seen: seen.data, unseen: unseen.data });
-                    return [2 /*return*/];
-            }
-        });
-    }); });
-    socket.on('getlength', function (response) { return __awaiter(void 0, void 0, void 0, function () {
-        var usersel, unseen;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    usersel = "SELECT  COUNT(a.messageid) as c FROM adminAnnoucment a\n        LEFT JOIN userAnnoucment u ON a.messageid = u.messageid\n        WHERE (a.receiver = '".concat(response.admno, "' OR a.receiver = 'all') \n          AND u.messageid IS NULL ORDER BY a.messageid DESC;\n          ");
-                    return [4 /*yield*/, Promise.all([sqlQueryStatus(usersel)])];
-                case 1:
-                    unseen = (_a.sent())[0];
-                    console.log("length :", unseen.data[0].c);
-                    socket.emit('getlength', { unseen: unseen.data[0].c });
-                    return [2 /*return*/];
-            }
-        });
-    }); });
-    socket.on("admin", function (response) { return __awaiter(void 0, void 0, void 0, function () {
-        var _i, _a, admno, insert, i, j;
-        return __generator(this, function (_b) {
-            console.log(response.message);
-            if (Array.isArray(response.reciver) === true) {
-                for (_i = 0, _a = response.reciver; _i < _a.length; _i++) {
-                    admno = _a[_i];
-                    insert = "INSERT INTO adminAnnoucment (message,sender, receiver) VALUES ('".concat(response.message, "','").concat(response.sender, "','").concat(admno, "');");
-                    sqlQueryUpdate(insert);
-                }
-            }
-            else {
-                console.log('send aa array');
-                return [2 /*return*/];
-            }
-            if (response.reciver[0] === "all") {
-                console.log('emited :', response.message);
-                io.emit("notice", { "message": response.message });
-            }
-            else {
-                for (i = 0; i < response.reciver.length; i++) {
-                    for (j = 0; j < dbActive.length; j++) {
-                        if (dbActive[j].admno === response.reciver[i]) {
-                            console.log(j, 'passed ', dbActive[j]);
-                            io.to(dbActive[j].socketid).emit("notice", response.message);
-                        }
-                    }
-                }
-            }
-            return [2 /*return*/];
-        });
-    }); });
-    socket.on("disconnect", function () {
-        for (var i = 0; i < dbActive.length; i++) {
-            if (dbActive[i].socketid == socket.id) {
-                dbActive.splice(i, 1);
-            }
-        }
-        console.log("A user disconnected", dbActive);
-    });
-});
-io.engine.on("connection_error", function (err) {
-    console.log(err.req); // the request object
-    console.log(err.code); // the error code, for example 1
-    console.log(err.message); // the error message, for example "Session ID unknown"
-    console.log(err.context); // some additional error context
-});
-// const PORT = process.env.PORT || 443;
-httpServer.listen(3000, function () {
-    console.log("server is running on port localhost:434");
-});
--- src/expsock.ts
+++ src/expsock.ts
@@ -15,6 +15,122 @@ interface sdb {
 }
 const dbActive:sdb[] = [];
 
+
+io.on("connection", (socket) => {
+    console.log("A user connected");
+    
+    socket.on("register", (response: { admno: string}) => {
+        removeDup(dbActive,response.admno)
+        if(response.admno!==undefined){
+
+            dbActive.push({
+                admno: response.admno,
+                socketid: socket.id,
+            })
+        }
+        console.log("active user", dbActive);
+    });
+    
+    socket.on("seen",(response: {admno: string,name:string,message:string,messageid:string}) => {
+        const insert = `INSERT INTO userAnnoucment (admno,name,messaged,messageid) VALUES ('${response.admno}','${response.name}','${response.message}','${response.messageid}');`
+        sqlQueryUpdate(insert);
+    })
+    
+    socket.on('getchat',async (response: {admno: string}) => {
+        const admno= `SELECT  a.messageid, a.message, a.receiver, a.sender, a.date , a.time  FROM adminAnnoucment a
+        LEFT JOIN userAnnoucment u ON a.messageid = u.messageid
+        WHERE (a.receiver = '${response.admno}' OR a.receiver = 'all') ORDER BY a.messageid DESC;;
+                        `
+        const usersel =`SELECT  a.messageid, a.message, a.receiver, a.sender, a.date , a.time  FROM adminAnnoucment a
+        LEFT JOIN userAnnoucment u ON a.messageid = u.messageid
+        WHERE (a.receiver = '${response.admno}' OR a.receiver = 'all') 
+          AND u.messageid IS NULL ORDER BY a.messageid DESC;
+          `        
+        const [seen,unseen]  = await Promise.all([sqlQueryStatus(admno),sqlQueryStatus(usersel)]);
+        console.log(seen,unseen);
+        socket.emit('getchat',{seen:seen.data,unseen:unseen.data});
+    })
+    
+    socket.on('getlength',async (response: {admno: string}) => {
+        
+        const usersel =`SELECT  COUNT(a.messageid) as c FROM adminAnnoucment a
+        LEFT JOIN userAnnoucment u ON a.messageid = u.messageid
+        WHERE (a.receiver = '${response.admno}' OR a.receiver = 'all') 
+          AND u.messageid IS NULL ORDER BY a.messageid DESC;
+          `        
+        const [unseen]:any  = await Promise.all([sqlQueryStatus(usersel)]);
+        console.log("length :",unseen.data[0].c)
+        
+        socket.emit('getlength',{unseen:unseen.data[0].c});
+    })
+
+
+    socket.on("admin", async(response: {message: string,reciver: string[],sender:string }) => {
+        console.log(response.message);
+        if(Array.isArray(response.reciver)===true){
+            for (let admno of response.reciver) {
+                const insert = `INSERT INTO adminAnnoucment (message,sender, receiver) VALUES ('${response.message}','${response.sender}','${admno}');`
+                sqlQueryUpdate(insert);
+            }
+        }else{
+            console.log('send aa array')
+            return;
+        }
+        if(response.reciver[0] === "all"){
+            console.log('emited :',response.message);
+            io.emit("notice", {"message":response.message});
+        }
+        else{
+            for (let i = 0; i < response.reciver.length; i++) {
+                for (let j = 0; j < dbActive.length; j++) {
+                    if (dbActive[j].admno === response.reciver[i]) {
+                        console.log(j,'passed ',dbActive[j]);
+                        io.to(dbActive[j].socketid).emit("notice", response.message);
+                    }
+                }
+            }
+        }
+    });
+
+    socket.on("disconnect", () => {
+        for (let i = 0; i < dbActive.length; i++) {
+            if (dbActive[i].socketid == socket.id) {
+                dbActive.splice(i, 1);
+            }
+        }
+        console.log("A user disconnected",dbActive);
+    });
+});
+
+io.engine.on("connection_error", (err) => {
+    console.log(err.req);      // the request object
+    console.log(err.code);     // the error code, for example 1
+    console.log(err.message);  // the error message, for example "Session ID unknown"
+    console.log(err.context);  // some additional error context
+});
+
+const PORT = process.env.PORT || 443;
+httpServer.listen(PORT, () => {
+    console.log("server is running on port localhost:434");
+});
+
+
+
+
+
+
+
+
+async function getLength(admno:string):Promise<number>{
+    const usersel =`SELECT  COUNT(a.messageid) as c FROM adminAnnoucment a
+    LEFT JOIN userAnnoucment u ON a.messageid = u.messageid
+    WHERE (a.receiver = '${admno}' OR a.receiver = 'all') 
+      AND u.messageid IS NULL ORDER BY a.messageid DESC;`
+    const [unseen]:any  = await Promise.all([sqlQueryStatus(usersel)]);
+    console.log("length :",unseen.data[0].c)
+    return unseen.data[0].c
+}
+
 function removeDup(arr:sdb[],admno:string) {
     for (let i = 0; i < arr.length; i++) {
         if (arr[i].admno === admno) {
@@ -115,109 +231,3 @@ async function sqlQueryStatus(query: string) {
         return { status: false, data: [] };
     }
 }
-async function getLength(admno:string):Promise<number>{
-    const usersel =`SELECT  COUNT(a.messageid) as c FROM adminAnnoucment a
-    LEFT JOIN userAnnoucment u ON a.messageid = u.messageid
-    WHERE (a.receiver = '${admno}' OR a.receiver = 'all') 
-      AND u.messageid IS NULL ORDER BY a.messageid DESC;`
-    const [unseen]:any  = await Promise.all([sqlQueryStatus(usersel)]);
-    console.log("length :",unseen.data[0].c)
-    return unseen.data[0].c
-}
-io.on("connection", (socket) => {
-    console.log("A user connected");
-    
-    socket.on("register", (response: { admno: string}) => {
-        removeDup(dbActive,response.admno)
-        if(response.admno!==undefined){
-
-            dbActive.push({
-                admno: response.admno,
-                socketid: socket.id,
-            })
-        }
-        console.log("active user", dbActive);
-    });
-    
-    socket.on("seen",(response: {admno: string,name:string,message:string,messageid:string}) => {
-        const insert = `INSERT INTO userAnnoucment (admno,name,messaged,messageid) VALUES ('${response.admno}','${response.name}','${response.message}','${response.messageid}');`
-        sqlQueryUpdate(insert);
-    })
-    
-    socket.on('getchat',async (response: {admno: string}) => {
-        const admno= `SELECT  a.messageid, a.message, a.receiver, a.sender, a.date , a.time  FROM adminAnnoucment a
-        LEFT JOIN userAnnoucment u ON a.messageid = u.messageid
-        WHERE (a.receiver = '${response.admno}' OR a.receiver = 'all') ORDER BY a.messageid DESC;;
-                        `
-        const usersel =`SELECT  a.messageid, a.message, a.receiver, a.sender, a.date , a.time  FROM adminAnnoucment a
-        LEFT JOIN userAnnoucment u ON a.messageid = u.messageid
-        WHERE (a.receiver = '${response.admno}' OR a.receiver = 'all') 
-          AND u.messageid IS NULL ORDER BY a.messageid DESC;
-          `        
-        const [seen,unseen]  = await Promise.all([sqlQueryStatus(admno),sqlQueryStatus(usersel)]);
-        console.log(seen,unseen);
-        socket.emit('getchat',{seen:seen.data,unseen:unseen.data});
-    })
-    
-    socket.on('getlength',async (response: {admno: string}) => {
-        
-        const usersel =`SELECT  COUNT(a.messageid) as c FROM adminAnnoucment a
-        LEFT JOIN userAnnoucment u ON a.messageid = u.messageid
-        WHERE (a.receiver = '${response.admno}' OR a.receiver = 'all') 
-          AND u.messageid IS NULL ORDER BY a.messageid DESC;
-          `        
-        const [unseen]:any  = await Promise.all([sqlQueryStatus(usersel)]);
-        console.log("length :",unseen.data[0].c)
-        
-        socket.emit('getlength',{unseen:unseen.data[0].c});
-    })
-
-
-    socket.on("admin", async(response: {message: string,reciver: string[],sender:string }) => {
-        console.log(response.message);
-        if(Array.isArray(response.reciver)===true){
-            for (let admno of response.reciver) {
-                const insert = `INSERT INTO adminAnnoucment (message,sender, receiver) VALUES ('${response.message}','${response.sender}','${admno}');`
-                sqlQueryUpdate(insert);
-            }
-        }else{
-            console.log('send aa array')
-            return;
-        }
-        if(response.reciver[0] === "all"){
-            console.log('emited :',response.message);
-            io.emit("notice", {"message":response.message});
-        }
-        else{
-            for (let i = 0; i < response.reciver.length; i++) {
-                for (let j = 0; j < dbActive.length; j++) {
-                    if (dbActive[j].admno === response.reciver[i]) {
-                        console.log(j,'passed ',dbActive[j]);
-                        io.to(dbActive[j].socketid).emit("notice", response.message);
-                    }
-                }
-            }
-        }
-    });
-
-    socket.on("disconnect", () => {
-        for (let i = 0; i < dbActive.length; i++) {
-            if (dbActive[i].socketid == socket.id) {
-                dbActive.splice(i, 1);
-            }
-        }
-        console.log("A user disconnected",dbActive);
-    });
-});
-
-io.engine.on("connection_error", (err) => {
-    console.log(err.req);      // the request object
-    console.log(err.code);     // the error code, for example 1
-    console.log(err.message);  // the error message, for example "Session ID unknown"
-    console.log(err.context);  // some additional error context
-});
-
-// const PORT = process.env.PORT || 443;
-httpServer.listen(3000, () => {
-    console.log("server is running on port localhost:434");
-});
